e5784a0674fae7cc5c5175481a250b7782178f51
長浜光輝 <nagahamm_@nagahamakoukinoMacBook-Pro.local>
2026-09-15 17:01:15 +0900

    fix(brew): remove unneeded brew doctor step
    - install_brew.sh から brew doctor の実行を削除
    
    Fixes #3

--- mac/brew/install_brew.sh
+++ mac/brew/install_brew.sh
@@ -3,9 +3,6 @@
 echo "Installing Homebrew ..."
 which brew >/dev/null 2>&1 || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-echo "Run brew doctor ..."
-which brew >/dev/null 2>&1 && brew doctor
-
 echo "Run brew update ..."
 brew update
 
